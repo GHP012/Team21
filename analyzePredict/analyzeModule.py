499d672db50d793bf36394e0169a303a2871fd30
OmphileL <12779027+OmphileL@users.noreply.github.com>
2020-02-10 12:38:51 +0200

    added my function to module

--- analyzePredict/analyzeModule.py
+++ analyzePredict/analyzeModule.py
@@ -1,2 +1,6 @@
 def play_mod():
     print('module works')
+
+def number_of_tweets_per_day(df):
+    # your code here
+    return df
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='analyzePredict', #the name package managers will use for your project, like numpy or pandas
-    version='0.1', #the current version number of your project
+    version='0.11', #the current version number of your project
     packages=find_packages(exclude=['tests*']),
     license='MIT', #name of the license you chose
     description='EDSA example python package', #one-sentence description of your package
