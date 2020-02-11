bad3d7868179bee2b00bcc104b78b522de9ef1d9
buleys <msibivee@gmail.com>
2020-02-11 13:16:49 +0200

    First draft of five number summary
    Calculation of five number summary

--- analyzePredict/analyzeModule.py
+++ analyzePredict/analyzeModule.py
@@ -1,2 +1,28 @@
 def play_mod():
     print('module works')
+    
+### START FUNCTION
+def five_num_summary(items):
+    items.sort()
+    new_items1 = []
+    new_items2 = []
+    if len(items) % 2 != 0:
+        median = items[len(items)//2]
+        new_items1 = items[0:items.index(median)]
+        new_items2 = items[items.index(median)+1:]
+        q1 = (new_items1[len(new_items1)//2] + new_items1[len(new_items1)//2 - 1]) / 2
+        q3 = (new_items2[len(new_items2)//2] + new_items2[len(new_items2)//2 - 1]) / 2
+        
+    elif len(items) % 2 == 0:
+        median = (items[len(items)//2] + items[len(items)//2 - 1]) / 2
+        new_items1 = items[0:int(len(items)/2)]
+        new_items2 = items[int(len(items)/2):]
+        q1 = (new_items1[len(new_items1)//2] + new_items1[len(new_items1)//2 - 1]) / 2
+        q3 = (new_items2[len(new_items2)//2] + new_items2[len(new_items2)//2 - 1]) / 2
+       
+    
+    summary = {"max": max(items),"median": round(median,2),"min": min(items), "q1": round(q1,2),"q3": round(q3,2)}
+    return summary
+    
+
+### END FUNCTION
