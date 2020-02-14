c131d6c4b2517f8e4d3444f713c249f7f09b2ddd
buleys <msibivee@gmail.com>
2020-02-14 13:39:42 +0200

    Updated function 2

---
+++ analyzePredict/Nombulelo_q2.py
@@ -0,0 +1,12 @@
+
+    ### START FUNCTION
+def five_num_summary(items):
+    summary_dict = {}
+    summary = np.quantile(items,[1, 0.5, 0, 0.25, 0.75])
+    list1 = ["max","median","min","q1","q3"]
+    for i in range(len(list1)):
+        summary_dict[list1[i]] = summary[i]
+                                 
+    return summary_dict
+
+### END FUNCTION
