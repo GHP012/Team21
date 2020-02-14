a852b91113b7e11b99d0526ef952bbe2e1959d73
Mmokwa <60466859+Mmokwa@users.noreply.github.com>
2020-02-14 09:26:42 +0200

    predict functions(function 3)
    trail for function 3 of the predict eskom functions

--- analyzePredict/analyzeModule.py
+++ analyzePredict/analyzeModule.py
@@ -1,2 +1,8 @@
-def play_mod():
-    print('module works')
+def date_parser(list_dates):
+  # [date[:10] for date in list_dates]
+    new_list = []
+    for date in list_dates:
+        
+        new_list.append(date[:10])
+        
+    return new_list
