5474b11e50043d33a5d70e89bb03630d3bca615d
Mmokwa <60466859+Mmokwa@users.noreply.github.com>
2020-02-14 10:38:48 +0200

    Fixed q1 errors flagged by report
    The grader wasn't updated

--- analyzePredict/analyzeModule.py
+++ analyzePredict/analyzeModule.py
@@ -4,8 +4,8 @@ def dictionary_of_metrics(items):
      dic = {}
      dic['mean'] = round(np.mean(items), 2)
      dic['median'] =round(np.median(items), 2)
-     dic['varience'] = round(np.var(items, ddof = 1),2)
-     dic['standard deviation'] =round(np.std(items, ddof = 1), 2)
+     dic['var'] = round(np.var(items, ddof = 1),2)
+     dic['std'] =round(np.std(items, ddof = 1), 2)
      dic['min'] = round(min(items), 2)
      dic['max'] =  round(max(items), 2)
      
