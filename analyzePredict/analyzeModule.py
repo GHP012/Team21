3179f3764dcef6bb56b310d7037b18ddac0db296
Mmokwa <60466859+Mmokwa@users.noreply.github.com>
2020-02-14 09:22:57 +0200

    Eskom functions(function 2)
    a trail of function 2 of the predict

--- analyzePredict/analyzeModule.py
+++ analyzePredict/analyzeModule.py
@@ -1,2 +1,13 @@
-def play_mod():
-    print('module works')
+import numpy as np
+
+def five_num_summ(items):
+    
+    items = np.array(items)
+    dic = {}
+    dic['max'] = round(max(items), 2)
+    dic['median'] = round(np.percentile(items, 50), 2)
+    dic['min'] = round(min(items), 2)
+    dic['q1'] = round(np.percentile(items, 25, interpolation = 'lower'), 2)
+    dic['q3'] = round(np.percentile(items, 75, interpolation = 'lower'), 2)
+                          
+    return dic
