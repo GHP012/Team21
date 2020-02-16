0ea508c08ba6077af2dc3fa80b7d0b7cb80c888e
Suvarna Chetty <suvarnachetty6@gmail.com>
2020-02-16 13:24:16 +0200

    uploaded function 7

--- analyzePredict/Function7.py
+++ analyzePredict/Function7.py
@@ -1,3 +1,5 @@
 def stop_words_remover(df):
-    # add code
-    return
+
+    splitter = df['Tweets'].apply(lambda x: x.lower().split())
+    df['Without Stop Words'] = splitter.apply(lambda x: [word for word in x if word not in stop_words_dict['stopwords']])
+    return df
