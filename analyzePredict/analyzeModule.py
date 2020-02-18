09815238411cacc0aee866564d8c8865d686a70a
Mmokwa <60466859+Mmokwa@users.noreply.github.com>
2020-02-18 15:27:12 +0200

    function_1
    A function that calculates the different measures as  shown.

--- analyzePredict/analyzeModule.py
+++ analyzePredict/analyzeModule.py
@@ -2,15 +2,16 @@ import numpy as np
 import pandas as pd
 def dictionary_of_metrics(items):
 
-""""Function that calculates the mean, median, variance, standard deviation, minimum and maximum of list in items.
+""" Function that calculates the mean, median, variance, standard deviation, minimum and maximum of list in items.
 
     Parameters:
     items(list): takes up a list of numbers.
 
     Returns:
-    dic: returning the mean, median, variance,standard deviation,minimum and maximum 
+    dictionary: returning the 'mean', 'median', 'var','std','min' , 'max',
+    corresponding to the mean, median, variance, standard deviation, minimum and maximum of the input list.
 
-""""
+"""
      dic = {}
      dic['mean'] = round(np.mean(items), 2)
      dic['median'] =round(np.median(items), 2)
