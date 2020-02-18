41b36c82ba8ce32e49d8264e3ade6a2647b88dd4
Mmokwa <60466859+Mmokwa@users.noreply.github.com>
2020-02-18 15:05:24 +0200

    function1_ Tudu
    1st function.

--- analyzePredict/analyzeModule.py
+++ analyzePredict/analyzeModule.py
@@ -1,6 +1,16 @@
 import numpy as np
 import pandas as pd
 def dictionary_of_metrics(items):
+
+""""Function that calculates the mean, median, variance, standard deviation, minimum and maximum of list in items.
+
+    Parameters:
+    items(list): takes up a list of numbers.
+
+    Returns:
+    dic: returning the mean, median, variance,standard deviation,minimum and maximum 
+
+""""
      dic = {}
      dic['mean'] = round(np.mean(items), 2)
      dic['median'] =round(np.median(items), 2)
@@ -10,13 +20,4 @@ def dictionary_of_metrics(items):
      dic['max'] =  round(max(items), 2)
      
      return dic 
-
-def play_mod():
-    print('module works')
-
-### START FUNCTION
-def word_splitter(df):
-    df['Split Tweets'] = df['Tweets'].str.split(' ')
-    return df
-### END FUNCTION
 
