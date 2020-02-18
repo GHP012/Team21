a5f937a739841a1cf6536a8446be053a9ed0cd34
buleys <msibivee@gmail.com>
2020-02-18 13:40:49 +0200

    Function 6
    Docstring included but not sure if they are correct

--- analyzePredict/analyzeModule.py
+++ analyzePredict/analyzeModule.py
@@ -1,2 +1,26 @@
 def play_mod():
     print('module works')
+### START FUNCTION
+def word_splitter(df):
+    """ Splits sentences in a dataframe's column and returns a list of the separated words           in lowercase
+    
+    Parameters:
+    -----------
+
+        df (dataframe): Dataframe of sentences in string format
+
+    Returns:
+    -----------
+
+        (list): List of the seperated words in lowercase 
+
+     Examples
+    -----------
+
+    >>> word_splitter(df)
+    @BongaDlulane Please send an email to mediades = [@bongadlulane, please, send, an, email to, mediades]
+
+    """
+    df['Split Tweets'] = list(map(lambda i: i.lower().split(),df.Tweets))
+    return df
+### END FUNCTION
