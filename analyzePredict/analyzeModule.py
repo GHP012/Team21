f424c9c24545e15cd25c0a6b89a3153802d11ba8
GHP012 <60350069+GHP012@users.noreply.github.com>
2020-02-19 09:49:25 +0200

    Update analyzeModule.py

--- analyzePredict/analyzeModule.py
+++ analyzePredict/analyzeModule.py
@@ -21,16 +21,15 @@ def dictionary_of_metrics(items):
 
 
      """
+    dic = {}
+    dic['mean'] = round(np.mean(items), 2)
+    dic['median'] =round(np.median(items), 2)
+    dic['var'] = round(np.var(items, ddof = 1),2)
+    dic['std'] =round(np.std(items, ddof = 1), 2)
+    dic['min'] = round(min(items), 2)
+    dic['max'] =  round(max(items), 2)
 
-     dic = {}
-     dic['mean'] = round(np.mean(items), 2)
-     dic['median'] =round(np.median(items), 2)
-     dic['var'] = round(np.var(items, ddof = 1),2)
-     dic['std'] =round(np.std(items, ddof = 1), 2)
-     dic['min'] = round(min(items), 2)
-     dic['max'] =  round(max(items), 2)
-
-     return dic
+    return dic
 
 #Function2
 def five_num_summary(items):
