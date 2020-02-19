d95aae9729e8a37acd1eb4c74e380dfbce50bf16
GHP012 <60350069+GHP012@users.noreply.github.com>
2020-02-19 10:02:37 +0200

    added new no 3

--- analyzePredict/analyzeModule.py
+++ analyzePredict/analyzeModule.py
@@ -58,31 +58,42 @@ def five_num_summary(items):
     return summary_dict
 
  #Function3
- def date_parser(dates):
-     """
-     Extracts Date from a list containing datetime series
+def date_parser(dates):
+    """
+    Function 3: Date Parser
+    Date Parser function that takes a list of datetime strings and returns only the
+    date in 'yyyy-mm-dd' format.
+    The function returns the date only from a list of items.
+    The input is a list of datetime strings and it outputs a list of dates.
+    The dates variable is a list of dates represented as strings.
+    The string contains the date in 'yyyy-mm-dd' format and the time in hh:mm:ss foramt.
+    The first three entries in this variable are:
+    dates[:3] == [
+        '2019-11-29 12:50:54',
+        '2019-11-29 12:46:53',
+        '2019-11-29 12:46:10'
+        ]
+    Function Specifications:
+    The function should take a list of strings as input.
+    Each string in the input list is formatted as 'yyyy-mm-dd hh:mm:ss'.
+    The function should return a list of strings where each element in the returned
+    list contains only the date in the 'yyyy-mm-dd' format.
+    Arg:
+    A list of datetime strings
+    Return:
+    A date or list of dates
+    Examples:
+    Input
+    [ '2019-11-29 12:50:54' , '2019-11-29 12:46:53' , '2019-11-29 12:46:10']
+    Output
+    [ '2019-11-29' , '2019-11-29' , '2019-11-29' ]
+    """
 
-     Arg:
-       list of datetimes
-
-     Return:
-       A date / list of dates
-
-     Examples:
-       Input
-       [ '2019-11-29 12:50:54' , '2019-11-29 12:46:53' , '2019-11-29 12:46:10']
-
-       Output
-       [ '2019-11-29' , '2019-11-29' , '2019-11-29' ]
-
-     """
-
-     result = []
-     for i in range(len(dates)):
-         res = dates[i][:10]
-         result.append(res)
-     return result
-
+    strdate = []                       # Define an empty list
+    for i in range(len(dates)):
+        strd = dates[i][:10]           # Accessing first 10 elements of string
+        strdate.append(strd)           # Inserts the new results into a list
+    return strdate
 
 #function4
 def extract_municipality_hashtags(df):
